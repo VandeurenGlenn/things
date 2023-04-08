d141e565c65ae0c0bfeee3c8e6541cc093837501
Glenn Vandeuren <vandeurenglenn@gmail.com>
2023-04-08 14:40:43 +0200

    Update build (clean www)

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,18 @@
 import nodeResolve from "@rollup/plugin-node-resolve";
 import typescript from "@rollup/plugin-typescript";
+import { readdir, unlink } from "fs/promises";
+import { join } from 'path'
+
+const cleanWWW = () => ({
+  name: 'cleanWWW',
+  buildStart: async () => {
+    const paths = await readdir('www')
+
+    for (const path of paths) {
+      if (!path.includes('index.html') && !path.includes('manifest.json') && !path.includes('assets')) await unlink(join('www', path))
+    }
+  }
+})
 
 export default [{
   input: ['./src/shell.ts', './src/views/todo.js', './node_modules/@leofcoin/storage/exports/browser-store.js'],
@@ -8,6 +21,7 @@ export default [{
     format: 'es'
   }],
   plugins: [
+    cleanWWW(),
     nodeResolve(),
     typescript()
   ]
